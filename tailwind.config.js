6e7894817f57e47d139553fb19b117b472005c7b
Rachit Khurana <rachitk@live.in>
2022-03-28 12:35:05 +0530

    Revert "[ADD] Header for desktop"
    This reverts commit 05f6d0dc9b17696f780d91b49588da13638ad6a6.

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,15 +4,7 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {
-      colors: {
-        primary: '#151315',
-        secondary: '#CEB2A5',
-      },
-      animation: {
-        "spin-slow": "spin 3s linear infinite",
-      },
-    },
+    extend: {},
   },
   plugins: [],
-};
+}
