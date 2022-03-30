3415b52d9c495f1457a7632c961e65f9f6bca9a5
Rachit Khurana <rachitk@live.in>
2022-03-30 13:47:16 +0530

    [ADD] Sidebar links from constants

--- components/common/WithSidebar.tsx
+++ components/common/WithSidebar.tsx
@@ -1,5 +1,6 @@
-import { DiscordPrimary, InstaPrimary, Logo, TwitterPrimary } from "assets/icons";
+import { Logo } from "assets/icons";
 import { A } from "components/atoms";
+import { socialLinks } from "constants/sidebar";
 
 type WithSidebarProps = {
   children?: JSX.Element | JSX.Element[] | string;
@@ -15,16 +16,18 @@ export const WithSidebar = (props: WithSidebarProps) => {
         py-6 border-r-2 border-secondary-1 border-opacity-5"
       >
         <Logo className="h-10 w-auto" />
-        <div className="flex flex-col gap-2 justify-center items-center">
-          <A href="https://www.instagram.com" className="px-2 py-2">
-            <InstaPrimary />
-          </A>
-          <A href="https://www.instagram.com" className="px-2 py-2">
-            <DiscordPrimary />
-          </A>
-          <A href="https://www.instagram.com" className="px-2 py-2">
-            <TwitterPrimary />
-          </A>
+        <div className="flex flex-col gap-3 justify-center items-center">
+          {socialLinks.map((element, index) => {
+            return (
+              <A
+                key={index}
+                href={element.href ?? "javascript.void(0)"}
+                className="px-2 py-2"
+              >
+                {element.image}
+              </A>
+            );
+          })}
         </div>
       </div>
       {props.children}
---
+++ constants/sidebar.tsx
@@ -0,0 +1,16 @@
+import { DiscordPrimary, InstaPrimary, TwitterPrimary } from "assets/icons";
+
+export const socialLinks = [
+  {
+    image: <InstaPrimary />,
+    href: 'https://www.instagram.com'
+  },
+  {
+    image: <DiscordPrimary />,
+    href: 'https://www.discord.com'
+  },
+  {
+    image: <TwitterPrimary />,
+    href: 'https://www.twitter.com'
+  },
+]
