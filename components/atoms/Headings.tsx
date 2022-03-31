d1bf9e024698661c1d7bfba7fc38e2607b7c54ca
Rachit Khurana <rachitk@live.in>
2022-03-31 15:25:39 +0530

    [FIX] section 1 - align + sizes

--- components/atoms/Headings.tsx
+++ components/atoms/Headings.tsx
@@ -7,7 +7,7 @@ type HeadingProps = {
 
 const H1component = (props: HeadingProps, ref: Ref<any>) => (
   <h1
-    className={`${props.className} text-5xl lg:text-7xl text-secondary-1 font-heading-serif font-extralight`}
+    className={`${props.className} text-4xl lg:text-5xl 2xl:text-6xl text-secondary-1 font-heading-serif font-extralight leading-loose`}
     ref={ref}
   >
     {props.children}
--- components/molecules/HeadingsLinkBgImageHero.tsx
+++ components/molecules/HeadingsLinkBgImageHero.tsx
@@ -11,9 +11,9 @@ type Props = {
 
 export const HeadingsLinkBgImageHero = (props: Props) => {
   return (
-    <article className="relative px-ws-section w-full h-screen grid content-end pb-[20%] lg:content-center lg:pb-0">
+    <article className="relative px-ws-section w-full h-screen grid content-end pb-[8%]">
       {props.image && <img src={props.image} className="absolute top-0 left-0 w-full h-full object-cover" alt="NF10X" />}
-      <div className="relative grid grid-flow-row-dense content-center gap-6 w-full lg:w-[90%] xl:w-[80%] 2xl:w-[70%] lg:pt-[15%]">
+      <div className="relative grid grid-flow-row-dense content-center gap-6 w-full lg:w-[90%] xl:w-[60%] 2xl:w-[60%]">
         {props.headings?.map((element, index) => {
           return (
             <H1
@@ -24,8 +24,8 @@ export const HeadingsLinkBgImageHero = (props: Props) => {
             </H1>
           );
         })}
-        <div className="mt-6">
-          <A href="/join">
+        <div className="mt-8">
+          <A href="/join" className="px-8 py-3">
             <span className="group text-secondary-1 text-mono whitespace-nowrap flex items-center text-lg">
               Join the Waitlist
               <span role="img" className="transition-all pl-2 group-hover:pl-5">
