8ae2547e478905b96290531192a4906fe707a0ea
avinash-tiwari <avikvvv@gmail.com>
2022-04-07 12:10:58 +0530

    refactor: modal code

--- components/atoms/GenericModal.tsx
+++ components/atoms/GenericModal.tsx
@@ -1,11 +1,23 @@
 import React from "react";
-import { useEffect } from "react";
 
-export const GenericModal = (props: any)=>{
-    console.log(props)
+interface Props {
+    showModal: boolean;
+    setShowModal: (showModal: boolean) => void;
+    children: JSX.Element | string;
+    title?: string;
+    header?: string;
+}
+
+export const GenericModal = ({
+        header,
+        showModal,
+        setShowModal,
+        children,
+        title
+}: Props)=>{
     return (
         <>
-            {props.showModal ? (
+            {showModal ? (
                 <>
                     <div
                         className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none"
@@ -14,14 +26,14 @@ export const GenericModal = (props: any)=>{
                             {/*content*/}
                             <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
                                 {/*header*/}
-                                {props.header ?
-                                    props.header : <div className="flex items-start justify-between p-5 border-b border-solid border-slate-200 rounded-t">
+                                {header ?
+                                    header : <div className="flex items-start justify-between p-5 border-b border-solid border-slate-200 rounded-t">
                                         <h3 className="text-3xl font-semibold">
-                                            {props.title ? props.title : "Attention"}
+                                            {title ? title : "Attention"}
                                         </h3>
                                         <button
                                             className="p-1 ml-auto bg-transparent border-0 text-black opacity-5 float-right text-3xl leading-none font-semibold outline-none focus:outline-none"
-                                            onClick={() => props.setShowModal(false)}
+                                            onClick={() => setShowModal(false)}
                                         >
                                             <span className="bg-transparent text-black opacity-5 h-6 w-6 text-2xl block outline-none focus:outline-none">
                                                 ×
@@ -30,23 +42,13 @@ export const GenericModal = (props: any)=>{
                                     </div>
                                 }
                                 {/*body*/}
-                                {props.body ? <props.body/> :
-                                    <div className="relative p-6 flex-auto">
-                                        <p className="my-4 text-slate-500 text-lg leading-relaxed">
-                                            I always felt like I could do anything. That’s the main
-                                            thing people are controlled by! Thoughts- their perception
-                                            of themselves! They're slowed down by their perception of
-                                            themselves. If you're taught you can’t do anything, you
-                                            won’t do anything. I was taught I could do everything.
-                                        </p>
-                                    </div>
-                                }
+                                {children}
                                 {/*footer*/}
                                 <div className="flex items-center justify-end p-6 border-t border-solid border-slate-200 rounded-b">
                                     <button
                                         className="text-red-500 background-transparent font-bold uppercase px-6 py-2 text-sm outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
                                         type="button"
-                                        onClick={() => props.setShowModal(false)}
+                                        onClick={() => setShowModal(false)}
                                     >
                                         Close
                                     </button>
