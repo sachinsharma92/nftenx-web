933869faffd5bdb7c939e7078b32de7c8d9ede88
Rachit Khurana <rachitk@live.in>
2022-04-26 18:22:42 +0530

    [FIX] events section responsive

--- components/molecules/TitleDescription-ImageTitleDescriptionBtnCards.tsx
+++ components/molecules/TitleDescription-ImageTitleDescriptionBtnCards.tsx
@@ -29,6 +29,54 @@ export const TitleDescription_ImageTitleDescriptionBtnCards = (
   //     );
   //   }
   // }
+
+  const getOneCard = (
+    element: Record<
+      string,
+      undefined | JSX.Element | string | MouseEventHandler
+    >
+  ) => {
+    return <div className="shrink-0 w-72 lg:w-72 3xl:w-96 bg-gray-200 bg-opacity-5 backdrop-blur-md p-5 border-secondary-1 border-opacity-10 border-[1px] rounded-3xl flex flex-col gap-2 items-center">
+      {element.image && (
+        <img
+          src={element.image as string}
+          alt={(element.description as string) ?? "NF10X event"}
+          className="w-full h-auto"
+        />
+      )}
+      {element.description && (
+        <p className="-mt-8 font-mono text-secondary-2 text-center">
+          {element.description}
+        </p>
+      )}
+      {element.buttonTitle && (
+        <button
+          onClick={(element.buttonAction as MouseEventHandler) ?? null}
+          className="text-sm font-mono mt-4 bg-secondary-2 bg-opacity-20 border-secondary-1 border-opacity-20 border-[1px] py-2 px-4 rounded-full"
+        >
+          {element.buttonTitle}
+        </button>
+      )}
+    </div>;
+  };
+
+  const getAllCards = () => {
+    const output = props.items?.map(
+      (
+        element: Record<
+          string,
+          undefined | JSX.Element | string | MouseEventHandler
+        >,
+        index
+      ) => {
+        return (
+          <React.Fragment key={index}>{getOneCard(element)}</React.Fragment>
+        );
+      }
+    );
+    return output;
+  }
+
   return (
     <article className="relative p-section py-14 lg:py-28 text-secondary-1 flex flex-col gap-8 justify-center items-center min-h-screen overflow-x-hidden">
       <div className="lg:w-3/5">
@@ -40,44 +88,12 @@ export const TitleDescription_ImageTitleDescriptionBtnCards = (
         )}
       </div>
 
-      <div className="flex flex-col md:grid mg:grid-flow-row-dense md:grid-cols-2 lg:grid-cols-3 gap-10 2xl:gap-16">
-        {props.items?.map(
-          (
-            element: Record<
-              string,
-              undefined | JSX.Element | string | MouseEventHandler
-            >,
-            index
-          ) => {
-            return (
-              <div
-                key={index}
-                className="w-full lg:w-72 3xl:w-96 bg-gray-200 bg-opacity-5 backdrop-blur-md p-5 border-secondary-1 border-opacity-10 border-[1px] rounded-3xl flex flex-col gap-2 items-center"
-              >
-                {element.image && (
-                  <img
-                    src={element.image as string}
-                    alt={(element.description as string) ?? "NF10X event"}
-                    className="w-full h-auto"
-                  />
-                )}
-                {element.description && (
-                  <p className="-mt-8 font-mono text-secondary-2 text-center">
-                    {element.description}
-                  </p>
-                )}
-                {element.buttonTitle && (
-                  <button
-                    onClick={
-                      (element.buttonAction as MouseEventHandler) ?? null
-                    }
-                    className="text-sm font-mono mt-4 bg-secondary-2 bg-opacity-20 border-secondary-1 border-opacity-20 border-[1px] py-2 px-4 rounded-full"
-                  >{element.buttonTitle}</button>
-                )}
-              </div>
-            );
-          }
-        )}
+      <div className="hidden md:grid md:grid-flow-row md:grid-cols-2 lg:grid-cols-3 gap-10 2xl:gap-16">
+        {getAllCards()}
+      </div>
+
+      <div className="w-full md:hidden -p-section flex flex-row flex-nowrap gap-5 overflow-x-auto pb-3">
+        {getAllCards()}
       </div>
 
       {/* <div className="hidden lg:flex flex-row justify-end gap-4 -mt-24">
