0ca227fcc49e59ba2c92f308405e4625b8f3138d
Rachit Khurana <rachitk@live.in>
2022-05-01 13:34:46 +0530

    [ADD] Heap tracker & useScript hooks

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,10 +2,12 @@ import '../styles/globals.scss'
 import type { AppProps } from 'next/app'
 import Web3Provider from 'web3-react'
 import connectors from '../utils/blockchainConnectors'
+import { Trackers } from 'components/common'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (<Web3Provider connectors={connectors} libraryName="ethers.js"  >
   <Component {...pageProps} />
+  <Trackers />
   </Web3Provider>)
 }
 
