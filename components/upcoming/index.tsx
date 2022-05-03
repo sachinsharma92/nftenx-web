4545126a203ac22c5c978e8327af3b543fdb7e50
Rachit Khurana <rachitk@live.in>
2022-05-03 19:33:57 +0530

    [CHANGE] Flickety to flex custom horizontal scroll

--- components/upcoming/index.tsx
+++ components/upcoming/index.tsx
@@ -2,33 +2,33 @@ import { Logo } from "assets/icons";
 import { ButtonOutline } from "components/atoms/ButtonOutline";
 import { InputPrimary } from "components/common/InputPrimary";
 import Image from "next/image";
-import Flickity from 'react-flickity-component';
+import Flickity from "react-flickity-component";
 import ImageUser from "../../assets/images/user-image.jpeg";
 import CommentIcon from "../../assets/icons/comment.svg";
 
 const flickityOptions = {
-  cellAlign: 'left',
-  wrapAround: true
-}
+  cellAlign: "left",
+  wrapAround: true,
+};
 
 const cardSliderData = [
   {
-    title: 'Fireside',
-    subtitle: 'Chats'
+    title: "Fireside",
+    subtitle: "Chats",
   },
   {
-    title: 'Fireside',
-    subtitle: 'Teardown'
+    title: "Fireside",
+    subtitle: "Teardown",
   },
   {
-    title: 'Fireside',
-    subtitle: 'Chats'
+    title: "Fireside",
+    subtitle: "Chats",
   },
   {
-    title: 'Fireside',
-    subtitle: 'Teardown'
+    title: "Fireside",
+    subtitle: "Teardown",
   },
-]
+];
 const UpcomingComponent = (props: Record<string, any>) => {
   return (
     <section className="upcoming-section">
@@ -36,21 +36,25 @@ const UpcomingComponent = (props: Record<string, any>) => {
         <div className="head-section">
           <div className="container">
             <h1 className="title1">Upcoming this month</h1>
-            <p className="description mt-3">Web3 is more than a sprouting new industry. It’s a re-architecting of <br /> the internet. New, unthought-of companies will emerge.</p>
+            <p className="description mt-3">
+              Web3 is more than a sprouting new industry. It’s a re-architecting
+              of <br /> the internet. New, unthought-of companies will emerge.
+            </p>
           </div>
         </div>
 
         <div className="card-slider">
-          <Flickity
+          {/* <Flickity
             className={'carousel'} // default ''
             elementType={'div'} // default 'div'
             options={flickityOptions} // takes flickity options {}
             disableImagesLoaded={false} // default false
             reloadOnUpdate // default false
-            static // default false
-          >
-            {cardSliderData.map((item) => (
-              <div className="card">
+            static // default false */}
+          {/* > */}
+          <div className="flex flex-row gap-5 flex-nowrap overflow-x-auto pr-5">
+            {cardSliderData.map((item, index) => (
+              <div key={index} className="card shrink-0">
                 <div className="info-section">
                   <div className="dotted-icon">
                     <span></span>
@@ -60,22 +64,27 @@ const UpcomingComponent = (props: Record<string, any>) => {
 
                   <div className="image-section">
                     <div className="comment-icon">
-                      <Image src={CommentIcon} />
+                      <Image src={CommentIcon} alt="" />
                     </div>
                     <div className="image-box">
-                      <Image src={ImageUser} />
+                      <Image src={ImageUser} alt="" />
                     </div>
                   </div>
                 </div>
                 <div className="content-sec">
                   <h3 className="title3 text-white">{item.title}</h3>
                   <h3 className="title3 text-golden">{item.subtitle}</h3>
-                  <p className="description mt-3">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt.Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod.</p>
+                  <p className="description mt-3">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+                    do eiusmod tempor incididunt.Lorem ipsum dolor sit amet,
+                    consectetur adipiscing elit, sed do eiusmod.
+                  </p>
                 </div>
                 <ButtonOutline className="btn-join">Know more</ButtonOutline>
               </div>
             ))}
-          </Flickity>
+          </div>
+          {/* </Flickity> */}
         </div>
       </div>
     </section>
