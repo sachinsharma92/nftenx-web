535ba02c0f7633dfa67baccfe472eccd1eb1e884
avinash-tiwari <avikvvv@gmail.com>
2022-05-03 13:57:25 +0530

    fix: on successfully connecting wallet, button is replaced with account

--- components/common/Header.tsx
+++ components/common/Header.tsx
@@ -1,36 +1,49 @@
 import { Logo, MenuSecondary } from "assets/icons";
 import { WalletConnectModal } from "components/walletConnectModal/walletConnectModal";
+import { METAMASK_CONSTANTS } from "constants/globalConstants";
 import { extraLinks, navLinks } from "constants/header";
 import Link from "next/link";
 import { useRouter } from "next/router";
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
+import { storage } from "utils/storage";
+import { hooks } from '../connectors/metamask/MetamaskConnector';
 
 export const Header = () => {
   const router = useRouter();
   const activeRoute = router.pathname.split("/")[1];
   const [showWalletConnectModal, setShowWalletConnectModal] = useState<Boolean | undefined>(false)
+  const [account, setAccount] = useState<string>('');
+  const { useAccounts } = hooks;
+  const accounts = useAccounts()
+
   const connectWalletHandler = ()=>{
     setShowWalletConnectModal(true)
   }
   // const headerBg = useRef<HTMLDivElement>(null);
 
-  // useEffect(() => {
-  //   const toggleHeaderBg = () => {
-  //     if (window.scrollY > 40)
-  //       headerBg.current?.classList.remove("-translate-y-full");
-  //     else headerBg.current?.classList.add("-translate-y-full");
-  //   };
-  //   if (window) window.addEventListener("scroll", toggleHeaderBg);
-  // }, []);
+  useEffect(() => {
+    if(accounts){
+      setAccount(accounts[0]);
+    }
+  }, [accounts]);
+
+  useEffect(() => {
+    const metamaskAddr = storage.get(METAMASK_CONSTANTS.ADDRESS);
+    if(metamaskAddr){
+      setAccount(metamaskAddr);
+    }
+  }, []);
+
+  const formatAccount = (accountNumber: string) => {
+    if(accountNumber){
+      return `${accountNumber.slice(0,4)}...${accountNumber.slice(-4)}`
+    }
+  }
 
   const linkPaddings = 'px-4 py-2 xl:px-6 xl:py-3';
 
   return (
     <header className="w-full z-[100] text-secondary px-section py-5 flex flex-row justify-between items-center fixed top-0 text-lg">
-      {/* <div
-        ref={headerBg}
-        className="w-full absolute bg-black bg-opacity-10 backdrop-blur-md h-full top-0 left-0 transition-all -translate-y-full"
-      ></div> */}
       <Logo className="block lg:hidden w-12 h-auto" />
       <button className="block lg:hidden"><MenuSecondary /></button>
       <nav className="hidden lg:block rounded-full bg-secondary-3 bg-opacity-10 backdrop-blur-md overflow-hidden z-[100]">
@@ -70,8 +83,11 @@ export const Header = () => {
           );
         })}
         <li className="inline-block">
-          <button onClick={connectWalletHandler} className="px-4 py-2 xl:px-6 xl:py-3 text-secondary-1 bg-secondary-3 bg-opacity-10 rounded-full inline-block backdrop-blur-md font-mono">
-            Connect Wallet
+          <button
+            onClick={account? ()=>null: connectWalletHandler}
+            className="px-4 py-2 xl:px-6 xl:py-3 text-secondary-1 bg-secondary-3 bg-opacity-10 rounded-full inline-block backdrop-blur-md font-mono"
+          >
+            {formatAccount(account) || 'Connect Wallet'}
           </button>
         </li>
       </ul>
