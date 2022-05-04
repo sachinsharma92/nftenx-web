d54169f449e1219bf5c8c13fecf54035bfaa3a91
avinash-tiwari <avikvvv@gmail.com>
2022-05-04 18:10:43 +0530

    add: static text n eligible page

--- components/mint/elligible.tsx
+++ components/mint/elligible.tsx
@@ -13,8 +13,8 @@ const ElligibleComponent = (props: Record<string, any>) => {
           <div className="content-section">
             <h1 className="title1 text-white">Congratulations!</h1>
             <h2 className="title1 text-gradient">You are now eligible to mint <br />the access pass.</h2>
-            {/* <p className="description mt-5">Current waittime till mint</p>
-            <div className="date">23 Days | 045:00 hrs</div> */}
+            <p className="description mt-5">Current waittime till mint</p>
+            <div className="date">23 Days | 045:00 hrs</div>
             <ButtonOutline className="btn-join mt-4" onClick={()=>router.push('/events')}>View all events</ButtonOutline>
           </div>
         </div>
