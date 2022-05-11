d660a554b2225e7a3e64b1da8bb4182f2556344b
Rachit Khurana <rachitk@live.in>
2022-05-11 20:32:16 +0530

    [ADD] Landing Page > New coin images

--- components/molecules/ButtonWithDescription.tsx
+++ components/molecules/ButtonWithDescription.tsx
@@ -10,12 +10,11 @@ type Props = {
 
 export const ButtonWithDescription = ({src, onClick, title, subTitle}: Props) => {
   return (
-        <div className="flex flex-col items-center gap-5">
-            <button onClick={onClick}>
-                <img src={src} alt="founder-coin"/>
+        <div className="relative flex flex-col items-center gap-5">
+            <img src={src} alt="founder-coin" className="absolute w-96 blur-3xl animate-spin-slow"/>
+            <button onClick={onClick} className="z-[1]">
+                <img src={src} alt="founder-coin" className="w-96"/>
             </button>
-            {/* <A href={href}>
-            </A> */}
             <div className="flex flex-col gap-3">
                 <P className="text-primary-light-2 font-heading-serif text-xl opacity-60">{title}</P>
                 <P className="text-primary-light-2 text-xs leading-relaxed opacity-60">{subTitle}</P>
