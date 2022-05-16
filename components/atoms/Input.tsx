5ec575a9f59de6cef079aeced3ef0620d1c47985
Rachit Khurana <rachitk@live.in>
2022-05-16 13:52:13 +0530

    [ADD] text input transparent type

--- components/atoms/Input.tsx
+++ components/atoms/Input.tsx
@@ -1,7 +1,19 @@
 import { HTMLProps } from "react";
 
+type PropType = {
+  styletype?: "transparent";
+};
 type InputPropType = HTMLProps<HTMLInputElement>;
 
-export const PrimaryInput = (props: InputPropType) => {
-  return <input {...props} className={`text-base text-secondary-1 py-4 px-8 rounded-full bg-white bg-opacity-5 border-[1px] border-white border-opacity-20 outline-none text-mono ${props.className}`} />
-}
+export const PrimaryInput = (props: InputPropType & PropType) => {
+  return (
+    <input
+      {...props}
+      className={`text-base text-secondary-1 py-4 px-8 rounded-full ${
+        props.styletype === "transparent"
+          ? "bg-transparent"
+          : "bg-white bg-opacity-5 border-[1px] border-white border-opacity-20"
+      } outline-none text-mono ${props.className}`}
+    />
+  );
+};
